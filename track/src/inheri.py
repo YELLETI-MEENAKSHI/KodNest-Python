bef1db0e2a992ffff754e9d5c1dc2669eebba779
Yelleti Meenakshi <yelletimeenakshi11@gmail.com>
2026-09-01 14:57:26 +0530

    inharitance

---
+++ track/src/inheri.py
@@ -0,0 +1,34 @@
+class Developer:
+    def work(self):
+        print("Developer is working")
+    
+    def attendMeeting(Self):
+        print("Dev is attending the meeting")
+
+class Python(Developer):
+    def work(Self):
+        print("Python dev is working")
+    
+    def doProject(self):
+        print("Completed the python project")
+
+class Java(Developer):
+    def work(self):
+        print("Java dev is working")
+
+    def doProjectjava(self):
+        print("Completed the java project")
+
+dev = Developer()
+dev.work()
+dev.attendMeeting()
+
+py = Python()
+py.work()
+py.attendMeeting()
+py.doProject()
+
+ja = Java()
+ja.work()
+ja.attendMeeting()
+ja.doProjectjava()
